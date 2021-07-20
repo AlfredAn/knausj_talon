f2a9e7db3b16b616ccabfda559da871839376858
Alfred Andersson <good.creeper@gmail.com>
2021-07-20 18:30:29 +0200

    pop to stop drag

--- code/mouse.py
+++ code/mouse.py
@@ -272,7 +272,10 @@ def show_cursor_helper(show):
 
 
 def on_pop(active):
-    if setting_mouse_enable_pop_stops_scroll.get() >= 1 and (gaze_job or scroll_job):
+    button_down = len(list(ctrl.mouse_buttons_down())) > 0
+    if button_down:
+        ctrl.mouse_click(button=0, up=True)
+    elif setting_mouse_enable_pop_stops_scroll.get() >= 1 and (gaze_job or scroll_job):
         stop_scroll()
     elif (
         not eye_zoom_mouse.zoom_mouse.enabled
