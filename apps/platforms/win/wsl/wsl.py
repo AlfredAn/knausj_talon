aa740be5bd79429a5b3cf635ef5b079f4451b4ce
Alfred Andersson <good.creeper@gmail.com>
2022-02-26 18:16:42 +0100

    test

--- apps/platforms/win/wsl/wsl.py
+++ apps/platforms/win/wsl/wsl.py
@@ -10,6 +10,8 @@ mod = Module()
 
 ctx = Context()
 
+mod.tag("wsl")
+
 # note: this context match is intentionally made more complex so that it is more specific
 # than the context defined in apps/win/windows_terminal/windows_terminal.py (and thereby
 # takes precedence).
